e20d21a22ee81f1fbeedf2f4015d44b26600e483
lincoln-boot <juliusmwas36@gmail.com>
2025-05-18 17:57:39 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$host = "sql313.infinityfree.com";
+$dbname = "if0_38969326_epiz_12345678_daylight";
+$username = "if0_38969326 ";
+$password = "oj12202003";
+$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+?>
