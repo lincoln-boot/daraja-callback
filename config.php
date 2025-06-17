643669b3734f25f4458ee1228f4f1f129d966fc6
lincoln-boot <juliusmwas36@gmail.com>
2025-06-17 11:20:51 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $host = "sql313.infinityfree.com";
-$dbname = "if0_38969326_epiz_12345678_daylight";
+$dbname = "if0_38969326_hotspot_voucherslin";
 $username = "if0_38969326 ";
 $password = "oj12202003";
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
